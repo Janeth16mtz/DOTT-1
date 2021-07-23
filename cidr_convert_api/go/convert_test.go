b4e6e959e5dc1f09c54aa563633b3bddb86fbdb5
Janeth16mtz <86743203+Janeth16mtz@users.noreply.github.com>
2021-07-23 09:37:24 -0500

    Update convert_test.go

--- cidr_convert_api/go/convert_test.go
+++ cidr_convert_api/go/convert_test.go
@@ -1,10 +1,9 @@
 package main
-package assert
 
 import (
 	"testing"
 
-	//"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestValidCidrToMask(t *testing.T) {
